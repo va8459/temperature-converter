0129a63ee90b5fa5d80325727d20f0a7ae23fe17
va8459 <90899469+va8459@users.noreply.github.com>
2021-10-06 14:51:45 +0530

    Add files via upload

---
+++ temperature converter.py
@@ -0,0 +1,5 @@
+celsius = float(input('Enter temperature in Celsius: '))  
+  
+# calculate temperature in Fahrenheit  
+fahrenheit = (celsius * 1.8) + 32  
+print('%0.1f  Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))  
